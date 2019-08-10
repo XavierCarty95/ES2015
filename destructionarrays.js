06801a17ff2059319564c3888c2534e30fd89fb3
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-08-10 08:50:00 -0400

    destructionarrays.js

---
+++ destructionarrays.js
@@ -0,0 +1,11 @@
+const companies = [
+  { name: 'Google', location: 'Mountain View' }, 
+  {name: 'FaceBook', location : 'Menlo Park' }, 
+  {name: 'Uber' , location: 'San Francisco'} 
+  ]
+
+
+
+const [ {location} ]  = companies
+
+location
